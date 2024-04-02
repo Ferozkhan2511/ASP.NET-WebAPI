13b923c390322a2afc4055a2842aef8f91f42b00
Ferozkhan2511 <68701069+Ferozkhan2511@users.noreply.github.com>
2024-04-02 15:28:05 +0530

    Update Startup.cs

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -41,24 +41,24 @@ namespace WebAPI
         {
             app.UseCors(options =>
             //options.WithOrigins("http://localhost:3000")
-            //  options.WithOrigins("http://localhost:1131")
+             options.WithOrigins("http://74.249.89.180:777")
             // options.WithOrigins(Environment.GetEnvironmentVariable("React_Front_end_Url"))
 
-            {
-            var frontEndUrl = Environment.GetEnvironmentVariable("React_Front_end_Url");
-            if (!string.IsNullOrEmpty(frontEndUrl))
-            {
-                options.WithOrigins(frontEndUrl)
+         //   {
+        //    var frontEndUrl = Environment.GetEnvironmentVariable("React_Front_end_Url");
+        //    if (!string.IsNullOrEmpty(frontEndUrl))
+        //    {
+        //        options.WithOrigins(frontEndUrl)
                        .AllowAnyHeader()
-                       .AllowAnyMethod();
-            }
-                else
-                {
+                       .AllowAnyMethod());
+    //        }
+    //            else
+    //            {
                     // Log a warning or handle the case where the environment variable is not set.
                     // For example:
                     // Console.WriteLine("Warning: React_Front_end_Url environment variable is not set.");
-                }
-            });
+       //         }
+       //     });
 
             if (env.IsDevelopment())
             {
